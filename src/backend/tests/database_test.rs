4eaeee68ec6c33a595c9dddfddf620ad313e7cf3
Aavtic <aadishmkerala@gmail.com>
2025-03-27 19:49:19 +0530

    Removed unwanted code

--- src/backend/tests/database_test.rs
+++ src/backend/tests/database_test.rs
@@ -2,25 +2,6 @@ use mongodb::Client;
 use mongodb::options::ClientOptions;
 use tokio;
 
-pub struct MongoClient {
-    client: Client,
-}
-
-impl MongoClient {
-    pub async fn connect() -> MongoClient {
-        let client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
-        let client = Client::with_options(client_options).unwrap();
-
-        return MongoClient {
-            client
-        };
-    }
-
-    pub async fn create_collection(&self, db_name: &str, collection_name: &str, ) {
-        let db = &self.client.database(db_name);
-        db.create_collection(collection_name).await.unwrap();
-    }
-}
 
 #[tokio::test]
 pub async fn connect() {
