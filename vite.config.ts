58fa9666ab69ed380a60c781dc9f211b9a5835cb
Aavtic <aadishmkerala@gmail.com>
2025-04-03 10:53:55 +0530

    Some changes to cope up with cookie and http stuff.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "localhost", // Ensure it's bound to localhost
+    port: 5173,        // Default Vite port
+    https: false       // Force HTTP instead of HTTPS
+  }
+});
